5930499b18eeac420af29ded834a282449af21ad
MilkyWayTop16 <milkywaytop16@gmail.com>
2025-08-16 00:12:29 +0300

    Delete OptimizationLagManager directory

--- OptimizationLagManager/src/main/java/org/gw/optimizationlagmanager/commands/OptimizationCommand.java
+++
@@ -1,7 +0,0 @@
-package org.gw.optimizationlagmanager.commands;
-
-import org.bukkit.command.CommandSender;
-
-public interface OptimizationCommand {
-    void execute(CommandSender sender, String[] args);
-}
